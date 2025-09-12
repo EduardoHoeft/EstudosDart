d205c14aa3274b7e729e973d45946062aca36416
Eduardo Hoeft <eduardohoeft@MacBook-Air-de-Eduardo.local>
2025-09-12 20:06:53 -0300

    resolução do exercício 3

---
+++ exer3.dart
@@ -0,0 +1,16 @@
+//3.Faça um algoritmo que receba um texto e exiba a quantidade de caracteres, sem gerar erro caso o texto não seja informado.
+
+import 'dart:io';
+
+void main() {
+
+  print('Digite um texto:');
+  String? texto = stdin.readLineSync();
+
+  if (texto == null || texto.isEmpty) {
+    print('texto não encontrado!');
+  } else {
+    int contCaracter = texto.length;
+    print('O número de caracteres: ${contCaracter}');
+  }
+}
