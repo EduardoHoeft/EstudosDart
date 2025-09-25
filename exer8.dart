ecd083770105b192c80afd0f7afc7e4ddfd70aaf
Eduardo Hoeft <eduardohoeft@MacBook-Air-de-Eduardo.local>
2025-09-25 12:35:50 -0300

    Resolução do exercício 8

---
+++ exer8.dart
@@ -0,0 +1,10 @@
+//8.Faça um algoritmo que crie uma variável com o valor inicial inteiro e, em seguida,
+//tente atribuir um texto a ela. Comente no código o que acontece.
+
+
+//ERROR: um valor do tipo "string" não aceita dentro var do tipo INT.
+void main() {
+  int valor = 10;
+
+  //valor = 'String';
+}
